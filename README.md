38c0c5909106fc57067e3438a6359a756dce4ac6
Adam Wood <adamtylerwood0511@gmail.com>
2019-10-16 16:56:39 -0400

    add jdbc test code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # PPA2
+
+//command to start jenkins in docker
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean
+
+//command to run sql database in docker
+1. The command below starts up a new postgress db container named ppa2db
+docker run -d -p 5432:5432 -v postgres-data:/var/lib/postgresql/data --name ppa2db POSTGRES_PASSWORD=password POSTGRES_DB=testdb postgres
+2. This command enters the container and creates the tables
+
+//command to run our program in docker
+
+//db info
+user = postgres
+password = password
+db name = testdb
+ip = 192.168.99.100
--- src/main/java/HW/ppa1Interface.java
+++ src/main/java/HW/ppa1Interface.java
@@ -1,17 +1,18 @@
 package HW;
 
 import java.util.Scanner;
+import java.sql.*;
 
 public class ppa1Interface {
-
+/*
 	public static void main(String[] args) {
 		//Initialize function object and scanner
 		ppa1Function ppa1 = new ppa1Function();
 		Scanner in = new Scanner(System.in);
-		
+
 		//Welcome Statement
 		System.out.println("Welcome to the PPA1 Function Interface. Please select a function or terminate the program.\n");
-		
+
 		//Loop to control interface interaction
 		int selection = 0;
 		do {
@@ -21,7 +22,7 @@ public class ppa1Interface {
 			System.out.println("3. Body Mass Index");
 			System.out.println("4. Retirement Age");
 			System.out.println("5. Exit");
-			
+
 			try {
 				selection = in.nextInt();
 			}
@@ -29,7 +30,7 @@ public class ppa1Interface {
 				//exit upon input error
 				selection = 5;
 			}
-			
+
 			if(selection == 1) {
 				System.out.println("\nYou have selected Split the Tip");
 				System.out.print("Enter the total bill: ");
@@ -37,7 +38,7 @@ public class ppa1Interface {
 				System.out.print("Enter the number of guests: ");
 				int guests = in.nextInt();
 				double[] ret = ppa1.splitTheTip(bill, guests);
-				
+
 				//decipher return
 				if(ret[0] == -1)
 					System.out.println("There cannot be zero guests.\n");
@@ -58,21 +59,44 @@ public class ppa1Interface {
 				System.out.println("Shortest distance between (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is " + ret + ".\n");
 			}
 			else if(selection == 3) {
-				
+
 			}
 			else if(selection == 4) {
-	
+
 			}
 			else if(selection != 5) {
 				System.out.println("Please enter a valid option.\n");
 			}
-			
+
 		}
 		while(selection != 5);
-		
+
 		System.out.println("\nGoodbye!");
-		
+
 		in.close(); //close scanner
 	}
+*/
+	public static void main(String[] args) {
+			try (Connection connection = DriverManager.getConnection("jdbc:postgresql://192.168.99.100:5432/testdb", "postgres", "password")) {
 
+					System.out.println("Java JDBC PostgreSQL Example");
+					// When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
+					// the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
+//          Class.forName("org.postgresql.Driver");
+
+					System.out.println("Connected to PostgreSQL database!");
+					Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery("SELECT * FROM test_table");
+					while (resultSet.next()) {
+							System.out.println(resultSet.getInt("id"), resultSet.getInt("num"));
+					}
+
+			} /*catch (ClassNotFoundException e) {
+					System.out.println("PostgreSQL JDBC driver not found.");
+					e.printStackTrace();
+			}*/ catch (SQLException e) {
+					System.out.println("Connection failure.");
+					e.printStackTrace();
+			}
+	}
 }
