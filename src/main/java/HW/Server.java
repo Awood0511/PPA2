d845071b72ae096ffadf3a03c9b5abce2801ef44
Adam Wood <adamtylerwood0511@gmail.com>
2019-10-19 18:55:58 -0400

    add setup for parsing URI

--- src/main/java/HW/Server.java
+++ src/main/java/HW/Server.java
@@ -26,11 +26,45 @@ public class Server {
     static class MainHandler implements HttpHandler{
         @Override
         public void handle (HttpExchange httpExchange) throws IOException{
-            String response = "Error";
-            httpExchange.sendResponseHeaders(200,response.getBytes().length);
-            OutputStream os= httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            String temp = "Method: " + httpExchange.getRequestMethod() + "\n";
+            temp = temp + "Path: " + httpExchange.getRequestURI();
+
+            //parse URI
+            //GET to split
+            if(false){
+
+            }
+            else if(false){
+
+            }
+            else if(false){
+
+            }
+            else if(false){
+
+            }
+            else{
+              home(httpExchange);
+            }
+        }
+
+        private void home(HttpExchange h) throws IOException{
+          String response = "Welcome to the http interface.\n";
+          response += "To see a list of all Split The Tip database entries go to /splitthetip\n";
+          response += "To see a list of all BMI database entries go to /bmi\n";
+          response += "To call the function Split the Tip send a post request with these arguments\n";
+          response += "\tvariable \tData Type\n";
+          response += "\tdinnerAmount \tdouble\n";
+          response += "\tguests \tint\n";
+          response += "To call the function BMI send a post request with these arguments\n";
+          response += "\tvariable \tData Type\n";
+          response += "\tfeet \tint\n";
+          response += "\tinches \tint\n";
+          response += "\tweight \tdouble\n";
+          h.sendResponseHeaders(200,response.getBytes().length);
+          OutputStream os= h.getResponseBody();
+          os.write(response.getBytes());
+          os.close();
         }
     }
 }
