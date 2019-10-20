d3b2d3db00c8832b0e219e02855fd48342957d10
carlos <carlos_247@live.com>
2019-10-19 23:40:24 -0400

    done with post response

--- src/main/java/HW/Server.java
+++ src/main/java/HW/Server.java
@@ -1,10 +1,14 @@
 package HW;
 
 import com.sun.net.httpserver.*;
+
+
 import java.io.*;
 import java.lang.reflect.Method;
 import java.net.*;
 import java.sql.*;
+import java.util.List;
+import java.util.Map;
 public class Server {
 
     HttpServer server;
@@ -41,10 +45,10 @@ public class Server {
             else if(method.equals("GET") && (uri.equals("/bmi"))){
               getBmi(httpExchange);
             }
-            else if(method.equals("POST") && (uri.equals("/splitthetip"))){
+            else if(method.equals("POST") && (uri.contains("/splitthetip"))){
               postSplitTheTip(httpExchange);
             }
-            else if(method.equals("POST") && (uri.equals("/bmi"))){
+            else if(method.equals("POST") && (uri.contains("/bmi"))){
               postBmi(httpExchange);
             }
             else{
@@ -92,11 +96,108 @@ public class Server {
         }
 
         private void postSplitTheTip(HttpExchange h) throws IOException{
-
+          try{
+            Map <String, List<String>> map= h.getRequestHeaders();
+            int guests=-1;
+            double dinnerAmount=-1;
+	          for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+              String temp= entry.getValue().toString();
+              temp=  temp.substring(temp.indexOf("[")+1,temp.indexOf("]")); 
+              if(entry.getKey().equalsIgnoreCase("dinnerAmount")){
+                dinnerAmount= Double.parseDouble(temp);
+              }
+              if(entry.getKey().equalsIgnoreCase("guests")){
+                guests=Integer.parseInt(temp);
+              }
+              
+              
+            }
+            if(guests ==-1 || dinnerAmount== -1 ){
+              String response="One or more request headers were not set correctly";
+              h.sendResponseHeaders(200,response.getBytes().length);
+              OutputStream os= h.getResponseBody();
+              os.write(response.getBytes());
+              os.close();
+            }
+            else{
+              String response;
+              ppa1Function ppa1Function= new ppa1Function();
+              double[] ret=ppa1Function.splitTheTipDB(dinnerAmount,guests,connection);
+              if (ret[0] == -1)
+              response=("There cannot be zero guests.\n");
+            else
+              response=("Each person pays $" + ret[0] + " and must unequally split a remainder of $" + ret[1] + ".\n");
+      
+            h.sendResponseHeaders(200,response.getBytes().length);
+            OutputStream os= h.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+				    }  
+          }
+          catch(Exception e){
+            String response="Could not parse request header variables";
+            h.sendResponseHeaders(404,response.getBytes().length);
+            OutputStream os= h.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+          }
         }
 
         private void postBmi(HttpExchange h) throws IOException{
-
+          try{
+            Map <String, List<String>> map= h.getRequestHeaders();
+            int feet=-1;
+            int inches=-1;
+            double weight=-1;
+	          for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+              String temp= entry.getValue().toString();
+              temp=  temp.substring(temp.indexOf("[")+1,temp.indexOf("]")); 
+              if(entry.getKey().equalsIgnoreCase("feet")){
+                  feet= Integer.parseInt(temp);
+              }
+              if(entry.getKey().equalsIgnoreCase("inches")){
+                inches=Integer.parseInt(temp);
+              }
+              if(entry.getKey().equalsIgnoreCase("weight")){
+                weight=Double.parseDouble(temp);
+              }
+              
+            }
+            if(inches ==-1 || feet==-1 || weight== -1 ){
+              String response="One or more request headers were not set correctly";
+              h.sendResponseHeaders(200,response.getBytes().length);
+              OutputStream os= h.getResponseBody();
+              os.write(response.getBytes());
+              os.close();
+            }
+            else{
+              ppa1Function ppa1Function= new ppa1Function();
+              String ret=ppa1Function.bodymassDB(feet, inches, weight,connection);
+              if (ret.equals("weightless"))
+                  ret=("You must enter a weight over or equal to 30 pounds.\n");
+              
+              else if (ret.equals("heightless"))
+                ret=("You must enter a height over or equal to 2 feet.\n");
+              
+              else {
+                int parseIndex = ret.indexOf('|');
+                ret=("Your BMI is " + ret.substring(parseIndex + 1) + " which is considered "
+                  + ret.substring(0, parseIndex) + ".\n");
+              }
+              String response=ret;
+              h.sendResponseHeaders(200,response.getBytes().length);
+              OutputStream os= h.getResponseBody();
+              os.write(response.getBytes());
+              os.close();
+				    }  
+          }
+          catch(Exception e){
+            String response="Could not parse request header variables";
+            h.sendResponseHeaders(404,response.getBytes().length);
+            OutputStream os= h.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+          }
         }
 
         private void home(HttpExchange h) throws IOException{
